77d794f8110a313708e95ae715c22ef9a68ccad9
Dmitrii Stepanov <89274620+StepanovCode@users.noreply.github.com>
2022-11-11 11:10:55 +0300

    Update init_bot.py

--- emplbot/bot/init_bot.py
+++ emplbot/bot/init_bot.py
@@ -6,5 +6,5 @@ print('1')
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token="5735379123:AAEjj0K7saEZu5iupM8wHUsOsAXXqf1qSZI")
+bot = Bot(token="...")
 dp = Dispatcher(bot, storage=MemoryStorage())
